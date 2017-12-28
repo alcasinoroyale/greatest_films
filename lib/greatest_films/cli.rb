30b6251e6464cb0448119165ba945f9170bdc595
Alex Zdatny <alexzdatny@gmail.com>
2017-12-28 23:28:45 +0000

    began the start method in cli class

--- lib/greatest_films/cli.rb
+++ lib/greatest_films/cli.rb
@@ -3,4 +3,19 @@ class GreatestFilms::CLI
   def call
     puts "Welcome to Rotten Tomatoes Greatest Comedies!"
   end
+
+  def start
+    input = nil
+    while input!= "exit"
+      puts ""
+      puts "Which film would you like to further explore? Enter 1 - 10."
+      puts "If you would like to exit, type 'exit'"
+      input = gets.strip
+      if input == "exit"
+        puts "Thank you for visiting Rotten Tomatoes Greatest Comedies. Goodbye!"
+        exit
+      end
+    end
+  end
+
 end
