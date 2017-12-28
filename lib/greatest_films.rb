0acd975d6dfa7e987223ef94c42eb63012e4de38
Alex Zdatny <alexzdatny@gmail.com>
2017-12-28 23:09:11 +0000

    altered environment file, added comedy and scraper class files

--- lib/greatest_films.rb
+++ lib/greatest_films.rb
@@ -1,2 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 require_relative "./greatest_films/version"
 require_relative "./greatest_films/cli"
+require_relative "./greatest_films/comedy"
+require_relative "./greatest_films/scraper"
