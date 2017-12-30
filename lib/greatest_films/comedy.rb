c95cce49191d67e9611e338659c84e928448a4cd
Alex Zdatny <alexzdatny@gmail.com>
2017-12-30 02:07:10 +0000

    added initialize method for index page attributes

--- lib/greatest_films/comedy.rb
+++ lib/greatest_films/comedy.rb
@@ -1,3 +1,16 @@
 class GreatestFilms::Comedy
   attr_accessor :name, :rating, :url, :movie_info, :director, :cast, :reviews
+
+  @@all = []
+
+def initialze(name = nil, rating = nil, url = nil)
+  @name = name
+  @rating = rating
+  @url = url
+  @@all << self
+end
+
+def self.all
+  @@all
+end
 end
