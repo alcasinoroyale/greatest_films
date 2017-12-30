964a1bda71faa766cab436590e6c35d363f7fe85
Alex Zdatny <alexzdatny@gmail.com>
2017-12-30 01:53:19 +0000

    connected cli and scraper classes

--- lib/greatest_films/cli.rb
+++ lib/greatest_films/cli.rb
@@ -1,6 +1,7 @@
 class GreatestFilms::CLI
 
   def call
+    GreatestFilms::Scraper.new.make_comedies
     puts "Welcome to Rotten Tomatoes Greatest Comedies!"
   end
 
@@ -11,11 +12,14 @@ class GreatestFilms::CLI
       puts "Which film would you like to further explore? Enter 1 - 10."
       puts "If you would like to exit, type 'exit'"
       input = gets.strip
+      comedy = GreatestFilms::Comedy.find(input.to_i)
       if input == "exit"
         puts "Thank you for visiting Rotten Tomatoes Greatest Comedies. Goodbye!"
         exit
+      elsif input.to_i > 0
+        if film = GreatestFilms::Comedy.find(input.to_i)
+          list_film_selection(film)
       end
     end
   end
-
 end
